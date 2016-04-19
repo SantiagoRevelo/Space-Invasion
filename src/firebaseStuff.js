b5cad36fad135df60955af895b993117af1cfb0c
sreveloc <sreveloc@gmail.com>
2016-04-19 17:06:46 +0200

    puntuacion online ordenada de mayor a menor

--- src/firebaseStuff.js
+++ src/firebaseStuff.js
@@ -1,45 +1,43 @@
 function FireBaseStuff() {
     this.SCORES_SHOWED = 10;
-    this.scoreListRef;
-    this.scoresListValues = [];
+    this.ref;
+    this.scoresListValues;
 };
 
 FireBaseStuff.prototype.preload = function() {
-    this.scoreListRef = new Firebase('https://spaceinvasion.firebaseio.com/');
-    
-     
-    //this.scoreListRef.push({ name:the_name, score:the_score });
-    
-    this.scoresListValues = this.scoreListRef.orderByChild('score').limitToLast(this.SCORES_SHOWED);
-    
-    // Add a callback to handle when a new score is added.
-    this.scoreListRef.on('child_added', function(newScoreSnapshot, prevScoreName) {
-        FireBaseStuff.prototype.handleScoreAdded(newScoreSnapshot, prevScoreName);
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    });
+    this.ref = new Firebase('https://spaceinvasion.firebaseio.com/');
+        
+    // Traemos las mejores puntuaciones ordenadas
+    //FireBaseStuff.scoresListValues = this.ref;    
     
     // Attach an asynchronous callback to read the data at our posts reference
-    this.scoreListRef.on("value", function(snapshot) {
-        console.log(snapshot.val());
-    }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
+    this.ref.orderByChild('score').limitToLast(this.SCORES_SHOWED).on("value", this.updateValues.bind(this), function (errorObject) {
+        console.log("Error de lectura de los datos de Firebase: " + errorObject.code);
     });
     
-    console.log('Online scores initialized');
+    console.log('Iniciadas las puntuaciones Online');
 };
 
-FireBaseStuff.prototype.handleScoreAdded = function(newScoreSnapshot, prevScoreName) {
-   // console.log('Scores Updated: ' + newScoreSnapshot.key() + ":" + newScoreSnapshot.val().name + "-" + newScoreSnapshot.val().score + "\n prevscorename: " + prevScoreName );
-};
 
-FireBaseStuff.prototype.addScore = function(theName, theScore) {
-        
+
+FireBaseStuff.prototype.updateValues = function(snapshot) {
+        this.scoresListValues = snapshot.val();
+        //console.log(snapshot.val());
+}
+
+FireBaseStuff.prototype.addScore = function(theName, theScore) {        
     this.scoreListRef.push({ name:theName, score:theScore });
 };
 
 FireBaseStuff.prototype.getLeaderBoard = function() {
+    var cleanList = [];
+    for(var i in fbs.scoresListValues) cleanList.push(fbs.scoresListValues[i]);
     
+    return cleanList.sort(this.compare); 
 };
+
+FireBaseStuff.prototype.compare = function(a, b) {
+    return (a.score > b.score)? -1 : (a.score < b.score)? 1 : 0;
+}
 
 fbs = new FireBaseStuff();
--- src/leaderboard.js
+++ src/leaderboard.js
@@ -1,5 +1,6 @@
 function Leaderboard() {	
     this.textStyle;
+    this.scoresText;
     
     this.firebutton;
     
@@ -10,6 +11,14 @@ function Leaderboard() {
     this.scoreListRef;
     this.scoresListValues;
     this.SCORES_SHOWED = 10;  
+    
+    //textUtils
+    this.content = [];
+    this.line = [];
+    this.wordIndex = 0;
+    this.lineIndex = 0;
+    this.wordDelay = 100;
+    this.lineDelay = 200;
 };
 
 Leaderboard.prototype.preload = function () {
@@ -19,7 +28,27 @@ Leaderboard.prototype.preload = function () {
 
 Leaderboard.prototype.create = function () {
 
-    this.textStyle = { font: "28px 'Press Start 2P', cursive", fill: "#C2C2C2", align: "center" };
+    this.textStyle = { font: "28px 'Press Start 2P', cursive", fill: "#C2C2C2", align: "left" };
+    
+    this.scoresText = this.add.text(0, 0, "", this.textStyle);
+    
+    var list = fbs.getLeaderBoard();
+    
+    var i = 0;
+    for (var key in list) {
+        if (i > 0) {
+            this.scoresText.text += "\n";
+            //this.content.push("\n");
+        }
+        this.scoresText.text += this.fitNameInSize(list[key].name, 8) + "   - - - -   " + list[key].score;
+        this.content.push(this.fitNameInSize(list[key].name, 8)  + "   - - - -   " + list[key].score);
+        i++;
+    }
+    
+    // posicionamos el texto en la pantalla.
+    this.scoresText.x = (this.world.width - this.scoresText.width ) * 0.5;
+    this.scoresText.y = (this.world.height - this.scoresText.height ) * 0.5;
+    this.scoresText.text = "";
     
     // pulsando una tecla, retornamos al menu.
     this.fireButton = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -28,8 +57,56 @@ Leaderboard.prototype.create = function () {
     //Add the CRT Filter
     this.singleFilter = new Phaser.Filter(this, null, this.cache.getShader('crtFilter'));
     this.singleFilter.setResolution(this.world.width, this.world.height);
-    this.stage.filters = [this.singleFilter];    
+    this.stage.filters = [this.singleFilter];
+    
+    this.nextLine();
 };
+
+Leaderboard.prototype.fitNameInSize = function(name, size) {
+     return (name + new Array(size + 1).join( ' ' )).substr(0,8);
+}
+
+
+Leaderboard.prototype.nextLine = function() {
+
+    if (this.lineIndex === this.content.length)
+    {
+        //  We're finished
+        return;
+    }
+
+    //  Split the current line on spaces, so one word per array element
+    this.line = this.content[this.lineIndex].split(' ');
+
+    //  Reset the word index to zero (the first word in the line)
+    this.wordIndex = 0;
+
+    //  Call the 'nextWord' function once for each word in the line (line.length)
+    this.time.events.repeat(this.wordDelay, this.line.length, this.nextWord, this);
+
+    //  Advance to the next line
+    this.lineIndex++;
+}
+
+Leaderboard.prototype.nextWord = function() {
+
+    //  Add the next word onto the text string, followed by a space
+    this.scoresText.text = this.scoresText.text.concat(this.line[this.wordIndex] + " ");
+
+    //  Advance the word index to the next word in the line
+    this.wordIndex++;
+
+    //  Last word?
+    if (this.wordIndex === this.line.length)
+    {
+        //  Add a carriage return
+        this.scoresText.text = this.scoresText.text.concat("\n");
+        //  Get the next line after the lineDelay amount of ms has elapsed
+        this.time.events.add(this.lineDelay, this.nextLine, this);
+    }
+}
+
+
 
 Leaderboard.prototype.update = function () {
     
